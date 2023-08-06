1b28154e6df55694f3e1bb22aafcc3a69ff145a9
G-soujanya <126576028+G-soujanya@users.noreply.github.com>
2023-08-06 21:44:58 +0530

    Add files via upload

---
+++ Day1/Name.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+class Name
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String name;
+        System.out.println("enter the person name");
+        name=s.nextLine();
+        System.out.println("welcome"+name);
+    }
+}
