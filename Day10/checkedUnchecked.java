5e0ce9b9b357ccb3b29d78f8e778fd0535457312
G-soujanya <126576028+G-soujanya@users.noreply.github.com>
2023-08-16 23:12:16 +0530

    Create checkedUnchecked.java

---
+++ Day10/checkedUnchecked.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+class checkedUnchecked
+{
+    static void fun1()
+    {
+        try{
+        System.out.println(10/0);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+    static void fun2()
+    {
+        fun1();
+    }
+    static void fun3()
+    {
+        fun2();
+    }
+    public static void main(String args[])
+    {
+        fun3();
+    }
+}
