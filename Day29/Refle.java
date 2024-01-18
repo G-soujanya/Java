3cfb95e08706cc08c24414f9bd150b4f70f9df20
G-soujanya <126576028+G-soujanya@users.noreply.github.com>
2024-01-19 01:50:09 +0530

    Add files via upload

---
+++ Day29/Refle.java
@@ -0,0 +1,22 @@
+public class Refle {
+    String id;
+    Refle()
+    {
+        System.out.println("hi");
+    }
+    Refle(int x)
+    {
+        System.out.println("hello");
+    }
+    public void print(int x)
+    {
+        System.out.println("hello world");
+    }
+
+
+
+    interface My
+    {
+        void print();
+    }
+}
