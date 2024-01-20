08451d401e2b629ec67623a75385abe1aec63a03
G-soujanya <126576028+G-soujanya@users.noreply.github.com>
2024-01-20 19:21:03 +0530

    Create Main.java

---
+++ Day30/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<Point> ts=new TreeSet<>();
+        ts.add(new Point(10,20));
+        ts.add(new Point(5,5));
+        System.out.println(ts);
+    }
+}
